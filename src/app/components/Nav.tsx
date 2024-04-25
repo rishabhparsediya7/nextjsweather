83bb0be7f32a9f9740d570921d0523c50d3c0f70
Rishabh Parsediya <parsediyarishabh@gmail.com>
2024-04-25 17:45:44 +0530

    update key prop

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -2,12 +2,12 @@ import { BarChart, Bell, Box, Home, LogOut, PiIcon, PieChart, Search, SunMoon } 
 
 export default function Nav() {
 
-    const IconsArray = [<Search size={16} />, <Home size={16} />, <BarChart size={16} />, <Bell size={16} />, <PieChart size={16} />, <Box size={16} />]
+    const IconsArray = [<Search key={'search'} size={16} />, <Home key={'home'} size={16} />, <BarChart key={'bar'} size={16} />, <Bell key={'bell'} size={16} />, <PieChart key={'pie'} size={16} />, <Box key={''} size={16} />]
     return (
         <div className="bg-[#09090a] flex flex-col justify-between items-center rounded-2xl h-[95%]">
             <ul className="space-y-6 p-4 flex flex-col items-center">
                 {IconsArray.map((icon, index) => (
-                    <li className="hover:rounded-xl hover:bg-[#1a1b1d] hover:p-3 p-3" key={index}>{icon}</li>
+                    <li className="hover:rounded-xl hover:bg-[#1a1b1d] hover:p-3 p-3" key={icon.key}>{icon}</li>
                 ))}
             </ul>
             <ul className="space-y-3 p-4">
